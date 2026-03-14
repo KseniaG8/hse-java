d3aafa97b8dbcee09ad106baec1a7b5d8d85da53
Ksenia Gorodkova <kdgorodkova@edu.hse.ru>
2026-03-14 13:05:08 +0300

    queue:

--- src/main/java/hse/java/lectures/lecture6/tasks/queue/BoundedBlockingQueue.java
+++ src/main/java/hse/java/lectures/lecture6/tasks/queue/BoundedBlockingQueue.java
@@ -1,25 +1,59 @@
 package hse.java.lectures.lecture6.tasks.queue;
 
 public class BoundedBlockingQueue<T> {
-
+    private final Object[] buffer;
+    private int head = 0;
+    private int tail = 0;
+    private int count = 0;
+    private final int capacity;
 
     public BoundedBlockingQueue(int capacity) {
-
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("Capacity error");
+        }
+        this.capacity = capacity;
+        this.buffer = new Object[capacity];
     }
 
-    public void put(T item) {
+    public void put(T item) throws InterruptedException {
+        if (item == null) {
+            throw new NullPointerException("Item error");
+        }
 
+        synchronized (this) {
+            while (count == capacity) {
+                wait();
+            }
+
+            buffer[tail] = item;
+            tail = (tail + 1) % capacity;
+            count++;
+
+            notifyAll();
+        }
     }
 
-    public T take() {
-        return null;
+    public T take() throws InterruptedException {
+        synchronized (this) {
+            while (count == 0) {
+                wait();
+            }
+
+            T item = (T) buffer[head];
+            buffer[head] = null;
+            head = (head + 1) % capacity;
+            count--;
+
+            notifyAll();
+            return item;
+        }
     }
 
     public int size() {
-        return 0;
+        return count;
     }
 
     public int capacity() {
-        return 0;
+        return capacity;
     }
 }
